647ceac5936802ce0eba7fa876d7d6a5c8047708
jiachenxu <943522184@qq.com>
2020-04-12 18:47:05 +0800

    数据源初始化

--- src/main/java/com/jiachen/search/SearchApplication.java
+++ src/main/java/com/jiachen/search/SearchApplication.java
@@ -1,8 +1,11 @@
 package com.jiachen.search;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@MapperScan("com.jiachen.datesource")
 @SpringBootApplication
 public class SearchApplication {
 
