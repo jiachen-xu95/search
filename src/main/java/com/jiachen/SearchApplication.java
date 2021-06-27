6398a4ef157cd4570c490a0e91338d3600a02440
jiachenxu <943522184@qq.com>
2021-06-27 21:39:39 +0800

    update

--- src/main/java/com/jiachen/SearchApplication.java
+++ src/main/java/com/jiachen/SearchApplication.java
@@ -1,10 +1,8 @@
 package com.jiachen;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@MapperScan("com.jiachen.datesource.dao")
 @SpringBootApplication
 public class SearchApplication {
 
